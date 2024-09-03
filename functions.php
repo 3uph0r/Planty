88b37cedd2028bf974b40e9a811a8b17fe313cfe
Michel Astijiano <astijiano.michel@orange.fr>
2024-09-04 00:30:41 +0200

    Suppression menu admin en anonyme

--- functions.php
+++ functions.php
@@ -5,3 +5,19 @@ function my_theme_enqueue_styles() {
 }
 add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
 ?>
+
+<?php
+function filtrer_elements_menu($items, $args) {
+    // Vérifie si l'utilisateur est connecté
+    if (!is_user_logged_in()) {
+        // Filtrer les éléments du menu pour retirer celui avec la classe 'admin'
+        $items = array_filter($items, function($item) {
+            return !in_array('admin', $item->classes);
+        });
+    }
+    return $items;
+}
+
+add_filter('wp_nav_menu_objects', 'filtrer_elements_menu', 10, 2);
+
+?>
